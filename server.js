0db51c142a96cd2a26a4030296b318b796d43400
Thana Chantrapong <thana.j@gmail.com>
2020-05-20 23:15:28 +0700

    1.4

--- server.js
+++ server.js
@@ -1,15 +1,66 @@
 const express = require('express')
 const app = express()
 
-// สำหรับ HTTP GET
-// app.get('/', (req, res) => {
-//   res.json({ message: 'Thana 1.2' })
-// })
+app.use(express.json())
 
-app.get('/hello/:message', (req, res) => {
-    const { params } = req
-    res.json({ message: 'Thana', params })
+// mock data
+const products = [
+    {
+      id: '1001',
+      name: 'Node.js for Beginners',
+      category: 'Node',
+      price: 990
+    },
+    {
+      id: '1002',
+      name: 'React 101',
+      category: 'React',
+      price: 3990
+    },
+    {
+      id: '1003',
+      name: 'Getting started with MongoDB',
+      category: 'MongoDB',
+      price: 1990
+    }
+  ]
+  
+  app.get('/products', (req, res) => {
+    res.json(products)
+  })
+  
+  app.get('/products/:id', (req, res) => {
+    const { id } = req.params
+    const result = products.find(product => product.id === id)
+    res.json(result)
+  })
+  
+  app.post('/products', (req, res) => {
+    const payload = req.body
+    res.json(payload)
+  })
+  
+  app.put('/products/:id', (req, res) => {
+    const { id } = req.params
+    res.json({ id })
+  })
+  
+  app.delete('/products/:id', (req, res) => {
+    const { id } = req.params
+    res.json({ id })
+  })
+  
+//---------------------------------------------
+
+// สำหรับ HTTP GET
+app.get('/', (req, res) => {
+  res.json({ message: 'Sample-nodejs-mongo 1.4' })
 })
+
+// app.get('/hello/:message', (req, res) => {
+//     const { params } = req
+//     res.json({ message: 'Thana', params })
+// })
 
 app.listen(9000, () => {
   console.log('Application is running on port 9000')
